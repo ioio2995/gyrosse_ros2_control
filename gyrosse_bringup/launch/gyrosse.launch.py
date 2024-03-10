54695a4c0503c8228f45424b655549683a6ad275
ORCIL <orcil.lionel@gmail.com>
2024-03-10 09:23:17 +0000

    ros2_control parameter

--- gyrosse_bringup/launch/gyrosse.launch.py
+++ gyrosse_bringup/launch/gyrosse.launch.py
@@ -1,90 +1,117 @@
-import os
-
-from ament_index_python.packages import get_package_share_directory
-
-
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition
 from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration
 
 from launch_ros.actions import Node
-
-import xacro
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-    gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
-             )
-    
-    gyrosse_bringup_path = os.path.join(
-        get_package_share_directory('gyrosse_bringup'))
 
-    gyrosse_description_path = os.path.join(
-        get_package_share_directory('gyrosse_description'))
-
-    robot_description_content = os.path.join(gyrosse_description_path,
-                              'urdf',
-                              'gyrosse_description.xacro')
-    
-
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [
+                    FindPackageShare("gyrosse_description"),
+                    "urdf",
+                    "gyrosse_description.xacro",
+                ]
+            ),
+        ]
+    )
     robot_description = {"robot_description": robot_description_content}
+
+    controllers_config_file = PathJoinSubstitution(
+        [
+            FindPackageShare("gyrosse_bringup"),
+            "config",
+            "gyrosse_controller.yaml",
+        ]
+    )
+
+    rviz_config_file = PathJoinSubstitution(
+        [
+            FindPackageShare("gyrosse_description"), 
+            "config", 
+            "config.rviz"
+        ]
+    )
+
+    joy_config_file = PathJoinSubstitution(
+        [
+            FindPackageShare("gyrosse_bringup"), 
+            "config", 
+            "ps4.config.yaml"
+        ]
+    )
+
+
+    gazebo_node = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [PathJoinSubstitution([FindPackageShare("gazebo_ros"), "launch", "gazebo.launch.py"])]
+        ),
+        launch_arguments={"verbose": "false"}.items(),
+    )
 
     control_node = Node(
         package="controller_manager",
         executable="ros2_control_node",
         parameters=[
             robot_description, 
-            os.path.join(gyrosse_bringup_path, 'config/gyrosse_controllers.yaml')],
-        output="both",
+            controllers_config_file],
+        output="both"
     )
 
     robot_state_pub_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
-        output="both",
         parameters=[robot_description],
+        output="both"
     )
 
     joint_state_broadcaster_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["joint_state_broadcaster", "-c", "/controller_manager"],
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
     )
 
     robot_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["gyrosse_base_controller", "-c", "/controller_manager"]
+        arguments=["gyrosse_base_controller",  "--controller-manager", "/controller_manager"]
     )
 
     rviz_node = Node(
-        package='rviz2',
-        executable='rviz2',
+        package="rviz2",
+        executable="rviz2",
         arguments=[
-            '-d',
-            os.path.join(gyrosse_description_path, 'config/config.rviz'),
+            "-d",
+            rviz_config_file,
         ],
-        output='screen',
+        output="screen",
     )
 
     joy_node = Node(
-        package='joy', executable='joy_node', name='joy_node',
+        package="joy", executable="joy_node", name="joy_node",
         parameters=[{
-            'deadzone': 0.05,
-            'autorepeat_rate': 30.0
+            "deadzone": 0.05,
+            "autorepeat_rate": 30.0
         }],
     )
 
     teleop_twist_joy_node = Node(
-        package='teleop_twist_joy',
-        executable='teleop_node',
-        name='teleop_twist_joy_node',
-        parameters=[os.path.join(gyrosse_bringup_path, 'config/ps4.config.yaml')],
+        package="teleop_twist_joy",
+        executable="teleop_node",
+        name="teleop_twist_joy_node",
+        parameters=[joy_config_file],
         remappings=[
-            ('/cmd_vel', '/gyrosse_base_controller/cmd_vel_unstamped'),
+            ("/cmd_vel", 
+             "/gyrosse_base_controller/cmd_vel_unstamped"),
         ]
     )
 
@@ -93,8 +120,7 @@ def generate_launch_description():
         robot_state_pub_node,
         joint_state_broadcaster_spawner,
         robot_controller_spawner,
-        gazebo,
-        rviz_node,
+        gazebo_node,
         joy_node,
         teleop_twist_joy_node,
     ])
